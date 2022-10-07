1318e4da48ed042343e96a3c3592e0db1331fe34
Trefania <tarirotrefania@gmail.com>
2022-10-07 08:22:48 -0700

    monty

--- monty.h
+++ monty.h
@@ -80,7 +80,8 @@ void nop(stack_t **stack, unsigned int line_nums);
 void add(stack_t **stack, unsigned int line_nums);
 void opcode(stack_t **stack, char *str, unsigned int line_nums);
 void mod(stack_t **stack, unsigned int line_nums);
-void div(stack_t **stack, unsigned int line_nums);
+/* void div(stack_t **stack, unsigned int line_nums); */
+/*void pchar(stack_t **stack, unsigned int line_nums); */
 void pstr(stack_t **stack, unsigned int line_nums __attribute__((unused)));
 void rotr(stack_t **stack, unsigned int line_nums);
 void sub(stack_t **stack, unsigned int line_nums);
